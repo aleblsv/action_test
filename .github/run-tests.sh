187baf8fbdc858b88dfeee0913d15c02216be9cc
Alex Belousov <alex@urbanautogrow.com>
2019-12-09 11:39:57 +0200

    Update run-tests.sh

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # Run the go-vet tool.
 echo "test 1 status"
+if $(curl -XGET 'http://weqwez1.com:8888/status' | grep  "200 OK" > /dev/null)
+then
+echo "OK"
+exit 1
+else
+echo "ERR"
+fi
 exit 0
